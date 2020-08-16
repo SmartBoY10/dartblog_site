0d7c4660a10417ec09ccfa94c5ca6adf2ba5442e
Qurol Abdujalilov <qurol.abduajalilov99@gmail.com>
2020-08-16 15:53:06 +0500

    Added files and changes required for deployment to heroku

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'ckeditor',
     'blog.apps.BlogConfig',
 ]
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'siteblog.urls'
